53ccd63d8db6af5c8394ff3a4128f26b37913449
Eli Pousson <eli.pousson@gmail.com>
2022-02-25 23:42:53 -0500

    feat: add units parameter to sf_bbox dist functions
    - feat: add units parameter to sf_bbox dist functions
    - feat: add sf_bbox_shift, sf_bbox_contract, and sf_bbox_expand functions

--- R/sf_bbox_misc.R
+++ R/sf_bbox_misc.R
@@ -2,28 +2,38 @@
 #'
 #' Simple bounding box functions that you can use to:
 #'
-#' - Measure distances (sf_bbox_dist, sf_bbox_xdist, sf_bbox_ydist, and sf_bbox_diagdist)
-#' - Get an aspect ratio (sf_bbox_asp)
-#' - Transform the coordinate reference system (sf_bbox_transform)
-#' - Convert a bounding box to a SQL style query (sf_bbox_to_lonlat_query), well
-#' known text (sf_bbox_to_wkt), or a simple feature object (sf_bbox_to_sf)
+#' - Measure distances ([sf_bbox_dist], [sf_bbox_xdist], [sf_bbox_ydist], and [sf_bbox_diagdist])
+#' - Get an aspect ratio ([sf_bbox_asp])
+#' - Transform the coordinate reference system ([sf_bbox_transform])
+#' - Convert a bounding box to a SQL style query ([sf_bbox_to_lonlat_query]), well
+#' known text ([sf_bbox_to_wkt]), or a simple feature object ([sf_bbox_to_sf])
+#' - Shift, expand, or contract a bounding box ([sf_bbox_shift], [sf_bbox_expand], [sf_bbox_contract])
 #'
 #' The only functions with additional parameters are sf_bbox_to_lonlat_query and sf_bbox_dist. All
 #' other functions only take a bbox parameter.
 #'
-#' @param bbox bounding box object
-#' @param coords query column names with coordinates. e,g, c("X", "Y") or c("lat", "lon")
+#' @param bbox A bounding box object.
+#' @param coords query column names with coordinates. e,g, c("X", "Y") or
+#'   c("lat", "lon")
 #' @param crs coordinate reference system to use for query; default 4326
 #' @param from,to xy pairs (e.g. c("xmax", "ymax) defining points to measure
 #'   distance from and to.
+#' @param units If `TRUE`, distance functions return with units. If `FALSE`
+#'   (default), distance functions return numeric values.
 #' @param orientation If `TRUE`, sf_bbox_asp returns a suggested orientation
 #'   based on aspect ratio (< 0.9 "portrait"; > 1.1 "landscape"; else "square");
 #'   defaults to `FALSE`.
+#' @param x_nudge,y_nudge Length 1 or 2 numeric vector; unitless.
+#' @param side one or more sides to shift: "top", "bottom", "left", "right", or
+#'   "all"
+#' @param dir If "in", contract the `bbox` by x_nudge and y_nudge. If "out",
+#'   expand the bbox by x_nudge and y_nudge. If dir is not `NULL`; absolute
+#'   values are used for x_nudge and y_nudge. Defaults to `NULL`.
 #' @md
 #' @name sf_bbox_misc
 NULL
 
-#' @aliases sf_bbox_asp
+#' @name sf_bbox_asp
 #' @rdname sf_bbox_misc
 #' @export
 sf_bbox_asp <- function(bbox, orientation = FALSE) {
@@ -44,10 +54,12 @@ sf_bbox_asp <- function(bbox, orientation = FALSE) {
   }
 }
 
-#' @aliases sf_bbox_dist
+#' @name sf_bbox_dist
 #' @rdname sf_bbox_misc
 #' @export
-sf_bbox_dist <- function(bbox, from, to) {
+#' @importFrom sf st_crs st_point st_distance
+#' @importFrom units as_units
+sf_bbox_dist <- function(bbox, from, to, units = FALSE) {
   dist <-
     sf::st_distance(
       sf::st_point(c(bbox[[from[1]]], bbox[[from[2]]])),
@@ -56,46 +68,54 @@ sf_bbox_dist <- function(bbox, from, to) {
 
   dist <- as.numeric(dist)
 
+  if (units) {
+    bbox_units <- sf::st_crs(bbox)$units_gdal
+    dist <- units::as_units(dist, value = bbox_units)
+  }
+
   return(dist)
 }
 
-#' @aliases sf_bbox_xdist
+#' @name sf_bbox_xdist
 #' @rdname sf_bbox_misc
 #' @export
-sf_bbox_xdist <- function(bbox) {
+sf_bbox_xdist <- function(bbox, units = FALSE) {
   # abs(bbox["xmax"] - bbox["xmin"]) # Get width
   sf_bbox_dist(
     bbox = bbox,
     from = c("xmin", "ymin"),
-    to = c("xmax", "ymin")
+    to = c("xmax", "ymin"),
+    units = units
   )
 }
 
-#' @aliases sf_bbox_ydist
+#' @name sf_bbox_ydist
 #' @rdname sf_bbox_misc
 #' @export
-sf_bbox_ydist <- function(bbox) {
+sf_bbox_ydist <- function(bbox, units = FALSE) {
   # abs(bbox["ymax"] - bbox["ymin"]) # Get height
   sf_bbox_dist(
     bbox = bbox,
     from = c("xmin", "ymin"),
-    to = c("xmin", "ymax")
+    to = c("xmin", "ymax"),
+    units = units
   )
 }
 
-#' @aliases sf_bbox_diagdist
+#' @name sf_bbox_diagdist
 #' @rdname sf_bbox_misc
 #' @export
 #' @importFrom sf st_bbox st_distance st_point
-sf_bbox_diagdist <- function(bbox) {
+sf_bbox_diagdist <- function(bbox, units = FALSE) {
   sf_bbox_dist(
     bbox = bbox,
     from = c("xmin", "ymin"),
-    to = c("xmax", "ymax")
+    to = c("xmax", "ymax"),
+    units = units
   )
 }
 
-#' @aliases sf_bbox_transform
+#' @name sf_bbox_transform
 #' @rdname sf_bbox_misc
 #' @export
 #' @importFrom sf st_crs st_transform st_bbox
@@ -142,3 +162,124 @@ sf_bbox_to_lonlat_query <- function(bbox, coords = c("longitude",  "latitude"), 
 
   glue::glue("({coords[1]} >= {bbox$xmin[[1]]}) AND ({coords[1]} <= {bbox$xmax[[1]]}) AND {coords[2]} >= {bbox$ymin[[1]]}) AND ({coords[2]} <= {bbox$ymax[[1]]})")
 }
+
+
+#' @rdname sf_bbox_misc
+#' @name sf_bbox_shift
+#' @export
+sf_bbox_shift <- function(bbox,
+                          x_nudge = 0,
+                          y_nudge = 0,
+                          side = c("all", "top", "bottom", "left", "right"),
+                          dir = NULL) {
+  side <- match.arg(side, several.ok = TRUE)
+
+  if (is.character(dir)) {
+    dir <-
+      switch(dir,
+             "in" = c(1, -1),
+             "out" = c(-1, 1)
+      )
+  } else {
+    dir <- c(dir * -1, dir)
+  }
+
+  if ((length(x_nudge) == 1) && length(y_nudge) == 1) {
+    if (is.null(dir)) {
+      x_nudge <-
+        list(
+          "min" = x_nudge,
+          "max" = x_nudge
+        )
+
+      y_nudge <-
+        list(
+          "min" = y_nudge,
+          "max" = y_nudge
+        )
+    } else if (is.numeric(dir)) {
+      x_nudge <- abs(x_nudge)
+      y_nudge <- abs(y_nudge)
+
+      x_nudge <-
+        list(
+          "min" = x_nudge * dir[[1]],
+          "max" = x_nudge * dir[[2]]
+        )
+
+      y_nudge <-
+        list(
+          "min" = y_nudge * dir[[1]],
+          "max" = y_nudge * dir[[2]]
+        )
+    }
+  } else if ((length(x_nudge) == 2) && (length(y_nudge) == 2)) {
+    x_nudge <-
+      list(
+        "min" = x_nudge[[1]],
+        "max" = x_nudge[[2]]
+      )
+
+    y_nudge <-
+      list(
+        "min" = y_nudge[[1]],
+        "max" = y_nudge[[2]]
+      )
+  }
+
+  check_side <- function(x, side_opts) {
+    any(c(x, "all") %in% side_opts)
+  }
+
+  if (check_side("left", side)) {
+    bbox[["xmin"]] <- bbox[["xmin"]] + x_nudge[["min"]]
+  }
+
+  if (check_side("right", side)) {
+    bbox[["xmax"]] <- bbox[["xmax"]] + x_nudge[["max"]]
+  }
+
+  if (check_side("bottom", side)) {
+    bbox[["ymin"]] <- bbox[["ymin"]] + y_nudge[["min"]]
+  }
+
+  if (check_side("top", side)) {
+    bbox[["ymax"]] <- bbox[["ymax"]] + y_nudge[["max"]]
+  }
+
+  return(bbox)
+}
+
+#' @rdname sf_bbox_misc
+#' @name sf_bbox_contract
+#' @export
+sf_bbox_contract <- function(bbox,
+                             x_nudge = 0,
+                             y_nudge = 0) {
+
+
+  sf_bbox_shift(bbox = bbox,
+                x_nudge = x_nudge,
+                y_nudge = y_nudge,
+                side = "all",
+                dir = -1
+  )
+
+}
+
+#' @rdname sf_bbox_misc
+#' @name sf_bbox_expand
+#' @export
+sf_bbox_expand <- function(bbox,
+                           x_nudge = 0,
+                           y_nudge = 0) {
+
+  sf_bbox_shift(bbox = bbox,
+                x_nudge = x_nudge,
+                y_nudge = y_nudge,
+                side = "all",
+                dir = 1
+  )
+
+}
+
