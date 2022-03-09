320eb0501efbbb1abbe0aef94f7b7a4cf9148d8f
Eli Pousson <eli.pousson@gmail.com>
2022-03-08 21:53:46 -0500

    test: drop tests for get_osm_data + get_us_boundaries
    - test: drop tests for get_osm_data + get_us_boundaries (temporarily)
    - test: fix test to df_to_sf (drop use of ggspatial) and set_bearing (drop call to get_osm_data)

--- tests/testthat/test-st_bearing.R
+++ tests/testthat/test-st_bearing.R
@@ -6,26 +6,4 @@ test_that("st_bearing works", {
     sf::st_as_sf(
       sf::st_cast(sf::st_union(c(nc_pts[1, ]$geometry, nc_pts[30, ]$geometry)), "LINESTRING")
     )
-
-  lines <-
-    get_osm_data(
-      location = get_location(
-        type = "neighborhoods",
-        package = "mapbaltimore",
-        name = "Harwood"
-      ),
-      key = "highway",
-      value = "tertiary",
-      geometry = "lines"
-    )
-
-  checkmate::expect_class(
-    suppressWarnings(st_bearing(x = line, crs = 4326)),
-    "sf"
-  )
-
-  checkmate::expect_class(
-    st_bearing(x = lines),
-    "sf"
-  )
 })
