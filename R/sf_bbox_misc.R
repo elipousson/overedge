065b3930da4dc8c6ab435387ac08e1371d277537
Eli Pousson <eli.pousson@gmail.com>
2022-03-22 22:52:57 -0400

    fix: correct issue w/ sf_bbox_to_sf

--- R/sf_bbox_misc.R
+++ R/sf_bbox_misc.R
@@ -143,8 +143,8 @@ sf_bbox_transform <- function(bbox, crs) {
 sf_bbox_to_sf <- function(bbox, sf_col = "geometry") {
   bbox_sf <- sf::st_as_sf(sf_bbox_to_sfc(bbox))
 
-  if ((length(names(bbox_sf)) == 1) && names(bbox_sf) == "x") {
-    bbox_sf <- dplyr::rename(bbox_sf, "{sf_col}" := x)
+  if (!is.null(sf_col)) {
+    sf::st_geometry(bbox_sf) <- sf_col
   }
 
   return(bbox_sf)
