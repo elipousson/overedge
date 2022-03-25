f47a590079b52bfa18a04c7f56df2f69e923ac24
Eli Pousson <eli.pousson@gmail.com>
2022-03-24 21:41:33 -0400

    feat: add st_trim function

--- R/st_erase.R
+++ R/st_erase.R
@@ -1,9 +1,9 @@
 
 #' Erase an area of a simple feature object
 #'
-#' An extended version of `sf::st_difference` that uses the flip parameter to
-#' optionally call `sf::st_intersection` and combines and unions the second
-#' object by default.
+#' An extended version of [sf::st_difference] that uses the flip parameter to
+#' optionally call [sf::st_intersection] and combines and unions the second
+#' object by default. [st_trim] is [st_erase] with flip set to `TRUE`.
 #'
 #' @param x sf object
 #' @param y sf object
@@ -14,15 +14,29 @@
 #' @export
 #' @importFrom sf st_union st_combine st_intersection st_difference
 st_erase <- function(x, y, flip = FALSE, union = TRUE) {
-  if (union) {
+
+  if (!is_same_crs(x, y))
+    y <- st_transform_ext(x = y, crs = x)
+
+  if (union)
     y <- sf::st_union(sf::st_combine(y))
-  }
 
   if (flip) {
-    x <- suppressWarnings(sf::st_intersection(x, y))
+    x <- suppressMessages(suppressWarnings(sf::st_intersection(x, y)))
   } else {
     x <- suppressWarnings(sf::st_difference(x, y))
   }
 
   return(x)
+}
+
+#' @rdname st_erase
+#' @name st_trim
+st_trim <- function(x, y, union = TRUE) {
+  st_erase(
+    x = x,
+    y = y,
+    flip = TRUE,
+    union = union
+  )
 }
