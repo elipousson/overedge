8ed83c4a72da7795b388620246386ab903557656
Eli Pousson <eli.pousson@gmail.com>
2022-03-29 22:28:07 -0400

    fix: remove non-ASCII character from cli_ask

--- R/utils.R
+++ R/utils.R
@@ -205,22 +205,22 @@ theme_method <- function(x, method = NULL) {
 #' @importFrom rlang is_interactive
 #' @importFrom cli cli_abort cli_alert
 #' @importFrom utils menu
-cli_yeah <- function(
-  x,
-  yes = c("Yes", "Definitely", "For sure", "Yup", "Yeah", "I agree", "Absolutely"),
-  no = c("No way", "Not now", "Negative", "No", "Nope", "Absolutely not"),
-  n_yes = 1,
-  n_no = 2,
-  shuffle = TRUE,
-  .envir = parent.frame()
-) {
+cli_yeah <- function(x,
+                     yes = c("Yes", "Definitely", "For sure", "Yup", "Yeah", "I agree", "Absolutely"),
+                     no = c("No way", "Not now", "Negative", "No", "Nope", "Absolutely not"),
+                     n_yes = 1,
+                     n_no = 2,
+                     shuffle = TRUE,
+                     .envir = parent.frame()) {
   x <- glue::glue_collapse(x, "\n")
   x <- glue::glue(x, .envir = .envir)
 
   if (!rlang::is_interactive()) {
     cli::cli_abort(
-      c("User input required, but session is not interactive.",
-        "Query: {x}")
+      c(
+        "User input required, but session is not interactive.",
+        "Query: {x}"
+      )
     )
   }
 
@@ -239,7 +239,7 @@ cli_yeah <- function(
 
 #' @noRd
 #' @importFrom cli cli_alert
-cli_ask <- function(x, prompt = "❯ ") {
+cli_ask <- function(x, prompt = ">> ") {
   cli::cli_alert(x)
   readline(prompt = prompt)
 }
