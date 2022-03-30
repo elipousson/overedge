324be2c7fed344f8f510b687022e5c33070ca6ed
Eli Pousson <eli.pousson@gmail.com>
2022-03-29 22:39:45 -0400

    docs: update docs/pkgdown site

--- examples/sf_to_df.R
+++ examples/sf_to_df.R
@@ -1,6 +1,12 @@
 nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"))
-nc_df <- ggspatial::df_spatial(nc)
-df_to_sf(nc_df, coords = c("x", "y"))
-nc_df$xy <- paste(nc_df$x, nc_df$y, sep = ",")
 
+nc_coords <-
+  suppressWarnings(sf::st_coordinates(sf::st_centroid(nc)))
+
+nc_df <-
+  dplyr::bind_cols(sf::st_drop_geometry(nc), nc_coords)
+
+nc_df <- janitor::clean_names(nc_df)
+df_to_sf(nc_df, coords = c("x", "y"), remove_coords = TRUE)
+nc_df$xy <- paste(nc_df$x, nc_df$y, sep = ",")
 df_to_sf(nc_df, coords = "xy", into = c("lon", "lat"))
