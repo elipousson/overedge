47691a09680baceafbeec8bff7e5a2275f3008b6
Eli Pousson <eli.pousson@gmail.com>
2022-04-05 17:35:16 -0400

    refactor: add is_geom_pkg_installed function

--- R/utils-pkg.R
+++ R/utils-pkg.R
@@ -14,6 +14,27 @@ is_pkg_installed <- function(pkg, repo = NULL) {
   }
 }
 
+#' Is the package needed for this geom installed?
+#'
+#' @noRd
+is_geom_pkg_installed <- function(geom) {
+
+  # Check if packages are available for text/label geoms
+  if (geom %in% c("textsf", "labelsf")) {
+    return(is_pkg_installed("geomtextpath"))
+  }
+
+  # Check if packages are available for text/label geoms
+  if (geom %in% c("mark", "mapbox", "location", "context", "markers", "numbered")) {
+    return(is_pkg_installed(pkg = "birdseyeview", repo = "elipousson/birdseyeview"))
+  }
+
+
+  if (geom %in% c("text_repel", "label_repel")) {
+    return(is_pkg_installed("ggrepel"))
+  }
+}
+
 #' @noRd
 ls_pkg_data <- function(pkg, envir = .GlobalEnv) {
   utils::data(package = pkg, envir = envir)$results[, "Item"]
