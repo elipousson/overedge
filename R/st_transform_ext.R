e3987b903a22d9f30a2f0d44fce6394d538831fc
Eli Pousson <eli.pousson@gmail.com>
2022-04-05 17:34:56 -0400

    feat: add st_omerc function

--- R/st_transform_ext.R
+++ R/st_transform_ext.R
@@ -10,6 +10,9 @@
 #' @param class Class of object to return (`sf` or `bbox`). If x is an sf list,
 #'   the returned object remains a list but may be converted to `bbox` if class
 #'   = "sf".
+#' @param rotate If rotate is greater or less than 0, [st_transform_ext] calls
+#'   [st_omerc] and returns an object with the oblique mercator projection
+#'   passing the value of rotate to the gamma parameter of the projection.
 #' @return An `sf`, `sfc`, or `bbox` object transformed to a new coordinate
 #'   reference system.
 #' @seealso \code{\link[sf]{st_transform}},\code{\link[sf]{st_crs}}
@@ -18,7 +21,8 @@
 #' @importFrom sf st_transform st_crs
 st_transform_ext <- function(x = NULL,
                              crs = NULL,
-                             class = NULL) {
+                             class = NULL,
+                             rotate = 0) {
   if (is_sf_list(x, ext = TRUE)) {
     x <-
       purrr::map(
@@ -37,23 +41,52 @@ st_transform_ext <- function(x = NULL,
     is_sf(x, ext = TRUE)
   )
 
-  if (!is.null(crs) && !is_same_crs(x = x, y = crs)) {
-    # if x has a different crs than the sf object passed to crs
-
-    if (is_sf(crs, ext = TRUE)) {
-      crs <- sf::st_crs(x = crs)
-    }
-
-    if (is_bbox(x)) {
-      # If x is a bbox
-      x <- sf_bbox_transform(bbox = x, crs = crs)
-    } else {
-      # If x is an sf or sfc object
-      x <- sf::st_transform(x = x, crs = crs)
-    }
+  if (rotate != 0) {
+    crs <- NULL
+    x <- st_omerc(x, rotate = rotate)
   }
 
-  x <- as_sf_class(x, class = class)
+  if (is.null(crs) || is_same_crs(x = x, y = crs)) {
+    return(as_sf_class(x, class = class))
+  }
 
-  return(x)
+  # if x has a different crs than the sf object passed to crs
+  if (is_sf(crs, ext = TRUE)) {
+    crs <- sf::st_crs(x = crs)
+  }
+
+  if (is_bbox(x)) {
+    # If x is a bbox
+    x <- sf_bbox_transform(bbox = x, crs = crs)
+  } else {
+    # If x is an sf or sfc object
+    x <- sf::st_transform(x = x, crs = crs)
+  }
+
+  return(as_sf_class(x, class = class))
+}
+
+#' @name st_transform_omerc
+#' @rdname st_transform_ext
+#' @export
+#' @importFrom glue glue
+#' @importFrom sf st_transform
+st_omerc <- function(x, rotate = 0) {
+
+  coords <- st_coords(x, keep_all = FALSE, crs = 4326)
+
+  proj <- "omerc"
+  datum <- "WGS84"
+  units <- "m"
+
+  # alpha <- 0
+  # k_0 <- 0.7
+  # +alpha={alpha} +k_0={k_0}
+
+  crs <-
+    glue::glue(
+      "+proj={proj} +lat_0={coords$lat} +lonc={coords$lon} +datum={datum} +units={units} +no_defs +gamma={rotate}"
+    )
+
+  sf::st_transform(x = x, crs = crs)
 }
