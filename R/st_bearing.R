47da9fe91e01d00334508797e9bf4ef5a952b70a
Eli Pousson <eli.pousson@gmail.com>
2022-04-05 20:36:21 -0400

    refactor: rework st_bearing (may need testing) + add get_start_end_point

--- R/st_bearing.R
+++ R/st_bearing.R
@@ -34,17 +34,15 @@
 #' @export
 #' @importFrom dplyr bind_cols
 st_bearing <- function(x, dir = FALSE, crs = 4326) {
-  is_pkg_installed("lwgeom")
-  is_pkg_installed("geosphere")
+  pts <- get_start_end_point(x)
 
-  start <- sf_to_df(lwgeom::st_startpoint(x), crs = crs)
-  end <- sf_to_df(lwgeom::st_endpoint(x), crs = crs)
+  is_pkg_installed("geosphere")
 
   x_bearing <-
     vapply(
-      seq_len(length(start$lon)),
+      seq_len(length(pts$start$lon)),
       function(x) {
-        geosphere::bearing(p1 = start[x, ], p2 = end[x, ])
+        geosphere::bearing(p1 = pts$start[x, ], p2 = pts$end[x, ])
       }, 1.0
     )
 
@@ -53,4 +51,44 @@ st_bearing <- function(x, dir = FALSE, crs = 4326) {
   }
 
   dplyr::bind_cols(x, bearing = x_bearing)
+}
+
+#' Get a list of start and end points for an object
+#'
+#' @noRd
+get_start_end_point <- function(x, crs = 4326, class = "df") {
+  is_pkg_installed("lwgeom")
+
+  pts <-
+    list(
+      start = as_sf(lwgeom::st_startpoint(x)),
+      end = as_sf(lwgeom::st_endpoint(x))
+    )
+
+  if (!is.null(class) && class == "df") {
+    pts$start <- sf_to_df(pts$start, crs = crs)
+    pts$end <- sf_to_df(pts$end, crs = crs)
+  }
+
+  return(pts)
+}
+
+#' Create a LINESTRING based on start and end points
+#'
+#' @noRd
+get_start_end_line <- function(x) {
+  pts <- get_start_end_point(x, class = NULL)
+
+  pts_combined <-
+    purrr::map2(
+      pts$start,
+      pts$end,
+      ~ sf::st_combine(
+        c(.x, .y)
+      ) %>% sf::st_cast("LINESTRING")
+    ) # %>%
+  #   dplyr::bind_rows() %>%
+  #  as_sf()
+
+  return(pts_combined)
 }
