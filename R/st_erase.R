374ff7175ea553a9e085244182a6236af629edca
Eli Pousson <eli.pousson@gmail.com>
2022-04-19 23:16:42 -0400

    feat: export st_erase

--- R/st_erase.R
+++ R/st_erase.R
@@ -5,12 +5,11 @@
 #' optionally call [sf::st_intersection] and combines and unions the second
 #' object by default. [st_trim] is [st_erase] with flip set to `TRUE`.
 #'
-#' @param x sf object
-#' @param y sf object
-#' @param flip If TRUE, use st_intersection; if FALSE use st_difference,
-#'   Default: FALSE
-#' @param union If TRUE, use st_combine and st_union on y before applying
-#'   difference/intersection, Default: FALSE
+#' @param x,y sf objects
+#' @param flip If `TRUE`, use st_intersection; if `FALSE` use st_difference,
+#'   Default: `FALSE`
+#' @param union If `TRUE`, use [sf::st_combine] and [sf::st_union] on y before
+#'   applying difference/intersection, Default: `FALSE`
 #' @export
 #' @importFrom sf st_union st_combine st_intersection st_difference
 st_erase <- function(x, y, flip = FALSE, union = TRUE) {
@@ -44,6 +43,7 @@ st_erase <- function(x, y, flip = FALSE, union = TRUE) {
 
 #' @rdname st_erase
 #' @name st_trim
+#' @export
 st_trim <- function(x, y, union = TRUE) {
   st_erase(
     x = x,
